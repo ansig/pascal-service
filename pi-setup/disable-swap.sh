af7b15de8bc49fd95a4e8374deb7ba1dee5ef1f2
Björn Nyberg <xist@raidlineup.com>
2018-04-23 20:29:45 +0200

    update disable-swap.sh to add a working control group option

--- pi-setup/disable-swap.sh
+++ pi-setup/disable-swap.sh
@@ -5,5 +5,5 @@ sudo dphys-swapfile swapoff && sudo dphys-swapfile uninstall && sudo update-rc.d
 echo Adding " cgroup_enable=cpuset cgroup_enable=memory" to /boot/cmdline.txt
 sudo cp /boot/cmdline.txt /boot/cmdline_backup.txt
 # if you encounter problems, try changing cgroup_memory=1 to cgroup_enable=memory.
-orig="$(head -n1 /boot/cmdline.txt) cgroup_enable=cpuset cgroup_memory=1"
+orig="$(head -n1 /boot/cmdline.txt) cgroup_enable=cpuset cgroup_enable=memory"
 echo $orig | sudo tee /boot/cmdline.txt
